7bb24778bd12cb7651d8d592776684448f7f0a5e
Basil Dudka <dudka.vasia2010@gmail.com>
2017-05-21 22:36:50 +0300

    second try

--- public/login/login.component.js
+++ public/login/login.component.js
@@ -4,27 +4,27 @@ angular.
   module('login').
   component('login', {
     templateUrl: 'login/login.template.html',
-    controller:['$http', '$scope', function loginController($http, $scope) {
+    controller:['$http', '$scope', '$location', function loginController($http, $scope, $location) {
       // This object will be filled by the form
       $scope.username = $scope.password = '';
 
       // Register the login() function
-      // $scope.login = function(){
-      //   $http.post('/login', {
-      //     username: $scope.username,
-      //     password: $scope.password,
-      //   })
-      //   .success(function(user){
-      //     // No error: authentication OK
-      //     // $rootScope.message = 'Authentication successful!';
-      //     $location.url('/todo-list');
-      //   })
-      //   .error(function(){
-      //     // Error: authentication failed
-      //     // $rootScope.message = 'Authentication failed.';
-      //     $location.url('/login');
-      //   });
-      // };
+      $scope.login = function(){
+        $http.post('/login', {
+          username: $scope.username,
+          password: $scope.password,
+        })
+        .success(function(user){
+          // No error: authentication OK
+          // $rootScope.message = 'Authentication successful!';
+          $location.url('/todo-list');
+        })
+        .error(function(){
+          // Error: authentication failed
+          // $rootScope.message = 'Authentication failed.';
+          $location.url('/login');
+        });
+      };
     }
   ]
   });
